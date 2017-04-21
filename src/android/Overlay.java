08275ca0bb7c268c67dc87cc3f7ae59f5e4f8ece
z0rkuM <oalvarellos@gmail.com>
2017-04-21 19:06:16 +0200

    Fix Overlay.java

--- src/android/Overlay.java
+++ src/android/Overlay.java
@@ -45,10 +45,10 @@ public class Overlay extends CordovaPlugin {
 
 				Button overlayedButton = new Button(thisActitity);
 				overlayedButton.setText(args.getString(0));
-				overlayedButton.setOnTouchListener(thisActitity);
+				//overlayedButton.setOnTouchListener(thisActitity);
 				overlayedButton.setAlpha(0.0f);
 				overlayedButton.setBackgroundColor(0x55fe4444);
-				overlayedButton.setOnClickListener(thisActitity);
+				//overlayedButton.setOnClickListener(thisActitity);
 
 				WindowManager.LayoutParams params = new WindowManager.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.TYPE_SYSTEM_ALERT, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL, PixelFormat.TRANSLUCENT);
 				params.gravity = Gravity.LEFT | Gravity.TOP;
