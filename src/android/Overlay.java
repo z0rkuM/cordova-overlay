6413c7af5e45235f7b76ecc1d651c4fc231a60ae
z0rkuM <oalvarellos@gmail.com>
2017-04-21 19:09:45 +0200

    Doing overlays

--- src/android/Overlay.java
+++ src/android/Overlay.java
@@ -37,34 +37,40 @@ public class Overlay extends CordovaPlugin {
     }
 
 
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    public boolean execute(final String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
 		final Activity thisActitity = this.cordova.getActivity();
         cordova.getThreadPool().execute(new Runnable() {
             public void run() {
-                WindowManager wm = (WindowManager) thisActitity.getSystemService(Context.WINDOW_SERVICE);
+				try {
+					WindowManager wm = (WindowManager) thisActitity.getSystemService(Context.WINDOW_SERVICE);
 
-				Button overlayedButton = new Button(thisActitity);
-				overlayedButton.setText(args.getString(0));
-				//overlayedButton.setOnTouchListener(thisActitity);
-				overlayedButton.setAlpha(0.0f);
-				overlayedButton.setBackgroundColor(0x55fe4444);
-				//overlayedButton.setOnClickListener(thisActitity);
+					Button overlayedButton = new Button(thisActitity);
+					overlayedButton.setText(args.getString(0));
+					//overlayedButton.setOnTouchListener(thisActitity);
+					overlayedButton.setAlpha(0.0f);
+					overlayedButton.setBackgroundColor(0x55fe4444);
+					//overlayedButton.setOnClickListener(thisActitity);
 
-				WindowManager.LayoutParams params = new WindowManager.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.TYPE_SYSTEM_ALERT, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL, PixelFormat.TRANSLUCENT);
-				params.gravity = Gravity.LEFT | Gravity.TOP;
-				params.x = 0;
-				params.y = 0;
-				wm.addView(overlayedButton, params);
+					WindowManager.LayoutParams params = new WindowManager.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.TYPE_SYSTEM_ALERT, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL, PixelFormat.TRANSLUCENT);
+					params.gravity = Gravity.LEFT | Gravity.TOP;
+					params.x = 0;
+					params.y = 0;
+					wm.addView(overlayedButton, params);
 
-				View topLeftView = new View(thisActitity);
-				WindowManager.LayoutParams topLeftParams = new WindowManager.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.TYPE_SYSTEM_ALERT, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL, PixelFormat.TRANSLUCENT);
-				topLeftParams.gravity = Gravity.LEFT | Gravity.TOP;
-				topLeftParams.x = 0;
-				topLeftParams.y = 0;
-				topLeftParams.width = 0;
-				topLeftParams.height = 0;
-				wm.addView(topLeftView, topLeftParams);
-                callbackContext.success(); // Thread-safe.
+					View topLeftView = new View(thisActitity);
+					WindowManager.LayoutParams topLeftParams = new WindowManager.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.TYPE_SYSTEM_ALERT, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL, PixelFormat.TRANSLUCENT);
+					topLeftParams.gravity = Gravity.LEFT | Gravity.TOP;
+					topLeftParams.x = 0;
+					topLeftParams.y = 0;
+					topLeftParams.width = 0;
+					topLeftParams.height = 0;
+					wm.addView(topLeftView, topLeftParams);
+					callbackContext.success(); // Thread-safe.
+				}
+				catch (Exception ex) {
+					callbackContext.success("Not created");
+					return true;
+				}
             }
         });
         return true;
